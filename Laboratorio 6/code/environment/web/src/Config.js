5599fea22a3c9a62434e83d5fa1ccd325f39af19
OtavioKoike <otaviokoike07@gmail.com>
2023-05-07 15:32:34 -0300

    [Chore]: Finalizado Laboratorio 6

---
+++ Laboratorio 6/code/environment/web/src/Config.js
@@ -0,0 +1,12 @@
+//Variables to replace
+const poolId = 'us-east-1_ORuTl9BcW'
+const clientId = '5g5bphgjbfbr828kpig9ku8mkj'
+const api = 'https://7wyepaym8c.execute-api.us-east-1.amazonaws.com/Prod'
+
+const Config = {
+    UserPoolId: poolId,
+    AppClientId: clientId,
+    ApiURL: api + '/notes'
+}
+
+export default Config
